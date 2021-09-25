4d18a677f21b934c2954b166ec77b81813ee6b45
Amir <amir.daniel@bellbird.com.py>
2021-09-25 11:37:01 -0400

    add login POST route

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
 
 const mockUserData = [
     { name: 'Mark' },
@@ -23,9 +26,31 @@ app.get('/users/:id', function (req, res) {
         {
             success: true,
             message: 'got one user',
-            users: req.params.id
+            users: mockUserData[req.params.id]
         }
     )
+})
+
+app.post('/login', function (req, res) {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const mockUserName = "amir";
+    const mockUserPassword = "123123";
+
+    if (username === mockUserName && password === mockUserPassword) {
+        res.json({
+            success: true,
+            message: 'password and username match!',
+            token: 'encrypted token goes here'
+        });
+    } else {
+        res.json({
+            success: false,
+            message: 'password and username match!',
+            token: 'encrypted token goes here'
+        });
+    }
 })
 
 app.listen(8000, function () {
